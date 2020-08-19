912417aae5a48752085a89fb9637ba9170e98e8a
Caroline Jepps <carolinejepps@android-596c691a2d1213bc.localdomain>
2020-08-19 12:09:41 -0700

    Added validations and error test

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, presence, true
+  validates :last_name, presence, true
+  validates :hourly_rate, numericality, true {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates_associated :store
 
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality, true {only_integer: true, greater_than_or_equal_to: 0}
+  
 end
