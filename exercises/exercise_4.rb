7e06a108fc57d888e71dbc63596bd28d2d521c47
Caroline Jepps <carolinejepps@android-596c691a2d1213bc.localdomain>
2020-08-19 10:53:14 -0700

    Added new stores and searches

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,27 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts @mens_stores
+
+@mens_stores.each do |s|
+  puts s.name, s.annual_revenue
+end
+
+@womens_stores = Store.where("annual_revenue <= 1000000", womens_apparel: true)
+
+puts @womens_stores
+
+@womens_stores.each do |s|
+  puts s.name, s.annual_revenue
+end
