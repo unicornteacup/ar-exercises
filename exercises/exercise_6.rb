49faec92f9220fd2a1a62fdd21d2e6721fff92f9
Caroline Jepps <carolinejepps@android-596c691a2d1213bc.localdomain>
2020-08-19 11:46:56 -0700

    Added new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Bob", last_name: "Jones", hourly_rate: 40)
+
+@store1.employees.create(first_name: "Hannah", last_name: "Potter", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Jane", last_name: "Rogers", hourly_rate: 80)
+
+@store1.employees.create(first_name: "Ralph", last_name: "Wiggum", hourly_rate: 71)
