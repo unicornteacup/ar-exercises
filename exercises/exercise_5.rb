cb7243e2bae84b3cd4cc8e174865314f3cc0e66f
Caroline Jepps <carolinejepps@android-596c691a2d1213bc.localdomain>
2020-08-19 11:39:57 -0700

    Added count and averages revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+
+puts @total_revenue
+
+@average_revenue = Store.average(:annual_revenue)
+
+puts @average_revenue
+
+@stores_over_mil = Store.where("annual_revenue >= 1000000").count 
+
+puts @stores_over_mil
