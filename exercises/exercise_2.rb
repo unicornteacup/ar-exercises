896f316f9cb118718a5cdab2f1392451ce030af0
Caroline Jepps <carolinejepps@android-596c691a2d1213bc.localdomain>
2020-08-19 09:59:39 -0700

    Added variables for store 1 and 2 and changed name for store1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store2 = Store.second
+
+
+puts @store1.name
+
+# @store2 = Store.where(id: 2)
+
+@store1.name = 'Burnabee'
+@store1.save
+
+puts @store1.name
